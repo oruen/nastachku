3be3d87faabc0952f37e310e4a345a0ec953da2f
Nick Recobra <oruen@undev.ru>
2013-04-02 00:14:51 +0400

    Меншинсы с яндекса.

---
+++ mentions.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+require "bundler/setup"
+require "nokogiri"
+require "csv"
+require "open-uri"
+
+mentions = []
+0.upto(50) do |i|
+  url = "http://blogs.yandex.ru/search.rss?text=nastachku&p=#{i}"
+  items = []
+  until items.size > 0
+    sleep 2
+    items = Nokogiri::XML(open(url)).xpath("//item")
+  end
+  mentions.push *(items.map do |m|
+    [m.xpath("link")[0].content, Time.parse(m.xpath("pubDate")[0].content), m.xpath("description")[0].content.gsub("\n", " ")]
+  end)
+end
+
+CSV.open "mentions.txt", "wb", col_sep: "\t" do |f|
+  f << %w(link date content)
+  mentions.each do |mention|
+    f << mention
+  end
+end
+
