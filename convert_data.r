905537c1135b695afadc7e9b6db8e473bd6a3fe5
Nick Recobra <oruen@undev.ru>
2013-04-10 04:47:39 +0400

    Фильтрованные меншинсы.

--- convert_data.r
+++ convert_data.r
@@ -3,3 +3,8 @@
 users <- read.delim("./data/users.txt")
 users$created_at <- as.Date(users$created_at)
 write.csv(users, file="data/users2.txt", row.names=FALSE)
+
+mentions <- read.delim("./data/mentions.txt")
+mentions <- mentions[substr(mentions$content, 0, 4) != "RT @",]
+mentions$date <- as.Date(mentions$date)
+write.csv(mentions, file="data/mentions2.txt", row.names=FALSE)
